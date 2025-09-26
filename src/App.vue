77fb2836934b7dd626477596ffaed43cc0704a91
jota <juan.erices04@inacapmail.cl>
2025-09-25 23:21:31 -0300

    Hola Mundo desde el componente principal

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 
 <template>
   <router-view />
+  <h1 class="text-2xl text-center text-red-500 hover:text-red-800">Hola, soy el componente principal de
+    <span class="text-2xl underline font-medium">Smartlend</span>
+  </h1>
 </template>
 
 <style scoped></style>
