4e7e65881bdda4c19879a7a69e836e4b989fc420
jota <juan.erices04@inacapmail.cl>
2025-09-25 23:14:18 -0300

    Se modifica App.vue y se incluye el logo de la empresa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
+
 </script>
 
 <template>
-  <div class="bg-red-500 text-white p-4 text-center">
-    Si esto se ve rojo, Tailwind funciona correctamente
-  </div>
+  <router-view />
 </template>
 
 <style scoped></style>
