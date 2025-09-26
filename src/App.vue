ff55bb9167fa78e6d1292b5ae397c379db30d220
jota <juan.erices04@inacapmail.cl>
2025-09-25 22:39:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-red-500 text-white p-4 text-center">
+    Si esto se ve rojo, Tailwind funciona correctamente
+  </div>
+</template>
+
+<style scoped></style>
