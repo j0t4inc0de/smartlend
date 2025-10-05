429cc96ce6308214f4982fbbe42d7779fa33145a
Juan <juan.erices04@inacapmail.cl>
2025-10-05 18:17:39 -0300

    test: seccion de verde a azul

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <h1 class="text-2xl text-center text-red-500 hover:text-red-800">Hola, soy el componente principal de
     <span class="text-2xl underline font-medium">Smartlend - Actualizado</span>
   </h1>
-    <div style="background: green; color: white; padding: 10px; text-align: center;">
+    <div style="background: blue; color: white; padding: 10px; text-align: center;">
       ✅ Despliegue automático funcionando - {{ new Date().toLocaleString() }}
     </div>
 </template>
