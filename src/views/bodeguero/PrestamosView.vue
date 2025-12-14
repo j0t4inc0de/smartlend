eac0d78b7dddaad312b4a3e3a5880990095578a1
jota <juan.erices04@inacapmail.cl>
2025-12-13 22:29:19 -0300

    Mejora: Actualizar el campo de búsqueda para permitir búsqueda por código o usuario

--- src/views/bodeguero/PrestamosView.vue
+++ src/views/bodeguero/PrestamosView.vue
@@ -28,8 +28,7 @@
 
         <!-- Buscador de código -->
         <div class="relative flex-1 md:w-64">
-          <input v-model="codigoBusqueda" @keyup.enter="buscarPrestamo" type="text"
-            placeholder="Buscar por código (FI-JE111210)..."
+          <input v-model="busqueda" type="text" placeholder="Buscar por código o usuario..."
             class="w-full bg-gray-800 text-white pl-10 pr-4 py-2 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500 text-sm" />
           <svg class="w-5 h-5 text-gray-400 absolute left-3 top-2.5" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
